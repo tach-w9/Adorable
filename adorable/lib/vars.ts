bff04f899c2c8083a3c710c8f2b9273f67138ca5
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2026-02-20 14:35:19 -0800

    change default template repo

--- adorable/lib/vars.ts
+++ adorable/lib/vars.ts
@@ -1,4 +1,5 @@
-export const TEMPLATE_REPO = "https://github.com/freestyle-sh/freestyle-next";
+export const TEMPLATE_REPO =
+  "http://github.com/freestyle-sh/freestyle-base-nextjs-shadcn";
 export const WORKDIR = "/workspace";
 export const VM_PORT = 3000;
 export const MODEL = "gpt-5-mini";
